01a2816404b56a7333fbdca24a3879189e7127ce
jienoel <chenjienoel@163.com>
2014-12-11 15:19:32 +0800

    Initial Commit

---
+++ cj/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cj
+//
+//  Created by apple on 14-12-11.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
